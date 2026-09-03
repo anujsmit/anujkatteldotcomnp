59b1b6d42241bcbd03e452226a63f074f8a6e668
anujsmit <anujkattel16@gmail.com>
2026-09-03 18:50:04 +0530

    hello world

--- src/App.jsx
+++ src/App.jsx
@@ -17,7 +17,7 @@ import TermsOfUse from "./servex/TermsOfUse";
 import ServeX from "./servex/ServeX";
 
 import Navbar from "./components/Navbar";
-import LocationFetch from "./Pages/projects/Locationfetch";
+import LocationFetch from "./Pages/projects/Locationfetch.jsx";
 function App() {
   return (
     <Router>
